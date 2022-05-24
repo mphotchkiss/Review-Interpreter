f4968430f44ef9577a17c5c006b8064832c867ef
HotchkissLuLe <matthew.hotchkiss@lumenlearning.com>
2022-05-24 16:22:59 -0700

    incomplete file processing

--- categorize.cpp
+++ categorize.cpp
@@ -1,6 +1,35 @@
 #include <cstdio>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
 
-int main() {
+using namespace std;
+
+string tolower(string s);
+
+struct sentence {
+    bool positive;
+    vector<string> feature;
+};
+
+int main(int argc, char ** argv) {
+    ifstream fstream;
+    string sentence;
+    vector<struct sentence> sentences;
+    fstream.open(argv[1]);
+    if (fstream.is_open()) {
+        while (getline(fstream, sentence)) {
+            vector<string> feature;
+            sentence = tolower(sentence);
+
+        }
+    }
     return 0;
+}
+
+string tolower(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
 }
